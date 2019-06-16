00be69cb457a7e4e2dcfdc7166495d0649226444
merklan@gmail.com <277@249G1Thub!>
2019-06-16 18:26:45 -0500

    destroying the gradient

--- lib/user/ui/screens/signin_screen.dart
+++ lib/user/ui/screens/signin_screen.dart
@@ -14,9 +14,11 @@ class SignInScreen extends StatefulWidget {
 
 class _SignInScreenState extends State<SignInScreen> {
   UserBloc userBloc;
+  double screenWidth;
 
   @override
   Widget build(BuildContext context) {
+    screenWidth = MediaQuery.of(context).size.width;
     userBloc = BlocProvider.of(context);
     return _handleCurrentSession();
   }
@@ -38,31 +40,32 @@ class _SignInScreenState extends State<SignInScreen> {
   Widget signInGoogleUI() {
     return Scaffold(
         body: Stack(alignment: Alignment.center, children: <Widget>[
-          GradientBack("", null),
-          Column(
-              mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-            Text("Welcome \nThis is your travel app",
-                style: TextStyle(
-                    fontSize: 37,
-                    fontFamily: "Lato",
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold)),
-            ButtonGreen(
-                text: "Login with Gmail",
-                onPressed: () {
-                  userBloc.signOut();
-                  userBloc.signIn().then((FirebaseUser user) {
-                    userBloc.updateUserData(User(
-                      uid: user.uid,
-                      name: user.displayName,
-                      email: user.email,
-                      photoUrl: user.photoUrl
-                    ));
-                  });
-                },
-                width: 300.0,
-                height: 50.0)
-          ])
-        ]));
+      GradientBack(height: null),
+      Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+        Flexible(//to avoid text bigger than screen
+            child: Container(
+              width: screenWidth,
+                child: Text("Welcome \nThis is your travel app",
+                    style: TextStyle(
+                        fontSize: 37,
+                        fontFamily: "Lato",
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold)))),
+        ButtonGreen(
+            text: "Login with Gmail",
+            onPressed: () {
+              userBloc.signOut();
+              userBloc.signIn().then((FirebaseUser user) {
+                userBloc.updateUserData(User(
+                    uid: user.uid,
+                    name: user.displayName,
+                    email: user.email,
+                    photoUrl: user.photoUrl));
+              });
+            },
+            width: 300.0,
+            height: 50.0)
+      ])
+    ]));
   }
 }
--- lib/widgets/gradient_back.dart
+++ lib/widgets/gradient_back.dart
@@ -1,33 +1,39 @@
 import 'package:flutter/material.dart';
 
 class GradientBack extends StatelessWidget {
-
-
   String title = "Popular";
   double height = 0.0;
 
-  GradientBack(this.title, this.height);
+  GradientBack({Key key, this.height});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    if (height == null) {
+      height = screenHeight;
+    }
+
     return Container(
       height: height,
       decoration: BoxDecoration(
-        gradient: LinearGradient(
-            colors: [
-              Color(0xFF4268D3),
-              Color(0xFF584CD1)
-            ],
-          begin: FractionalOffset(0.2, 0.0),
-          end: FractionalOffset(1.0, 0.6),
-            stops: [0.0, 0.6],
-            tileMode: TileMode.clamp
-        )
-      ),
-
-
-      child: Text(
+          gradient: LinearGradient(
+              colors: [Color(0xFF4268D3), Color(0xFF584CD1)],
+              begin: FractionalOffset(0.2, 0.0),
+              end: FractionalOffset(1.0, 0.6),
+              stops: [0.0, 0.6],
+              tileMode: TileMode.clamp)),
+      child: FittedBox(
+          fit: BoxFit.none,
+          alignment: Alignment(-1.0, -0.8),
+          child: Container(
+            width: screenHeight * 3,
+            height: screenHeight,
+            decoration: BoxDecoration(
+                color: Color.fromRGBO(0, 0, 0, 0.5),
+                borderRadius: BorderRadius.circular(screenHeight / 2)),
+          )), /*Text(
         title,
         style: TextStyle(
           color: Colors.white,
@@ -37,9 +43,7 @@ class GradientBack extends StatelessWidget {
         ),
       ),
 
-      alignment: Alignment(-0.9, -0.6),
-
+        alignment: Alignment(-0.9, -0.6),*/
     );
   }
-
 }
